94313a2b3e5fa74604852f6f35fb2afdec43f154
Rupesh <2018uec1773@mnit.ac.in>
2024-06-30 21:35:32 +0530

    function update

--- src/utils.py
+++ src/utils.py
@@ -6,16 +6,10 @@ def load_csv_data_df(spark, file_path):
 def read_yaml(file_path):
     with open(file_path, 'r') as file:
         return yaml.safe_load(file)
-    
-'''def write_output(df,output_path):
-    df.write.format("csv").option("header","true").save(output_path)'''
+
 
 def write_output(df, file_path):
     """
-    Write data frame to csv
-    :param write_format: Write file format
-    :param df: dataframe
-    :param file_path: output file path
-    :return: None
+    Write data frame to csv in output folder
     """
     df.write.format('csv').mode("overwrite").option("header", "true").save(file_path)
